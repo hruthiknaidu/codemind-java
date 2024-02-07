f438b02327696470756400b318911871dd37af3b
hruthiknaidu <119839869+hruthiknaidu@users.noreply.github.com>
2024-02-07 10:13:19 +0530

    import java.util.*;  public class temp{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         double c=sc.nextDouble();         double f;         f=32+((c*9/5));         System.out.println(String.format("%.2f",f));     }
    }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ public class temp{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double c=sc.nextDouble();
+        double f;
+        f=32+((c*9/5));
+        System.out.println(String.format("%.2f",f));
+    } 
+
+}
